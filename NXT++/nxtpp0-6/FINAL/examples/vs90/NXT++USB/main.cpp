c2c5b0ba768e8e0964d0ca8350bc85bf154d0f93
samyungsenlee <samyungsenlee@localhost>
2010-03-15 14:00:41 -0400

    Add NXT++

---
+++ NXT++/nxtpp0-6/FINAL/examples/vs90/NXT++USB/main.cpp
@@ -0,0 +1,16 @@
+#include "NXT++.h"
+ 
+int main()
+{
+    Comm::NXTComm comm;
+    if(NXT::Open(&comm)) //initialize the NXT and continue if it succeeds
+    {
+        NXT::Motor::SetForward(&comm, OUT_A, 50); //turn both motors on 50% power
+        NXT::Motor::SetForward(&comm, OUT_C, 50);
+        Wait(2000); //wait 2 seconds
+        NXT::Motor::Stop(&comm, OUT_A, false); //stop both motors with no braking
+        NXT::Motor::Stop(&comm, OUT_C, false);
+        NXT::Close(&comm); //close the NXT
+    }
+    return 0;
+}
